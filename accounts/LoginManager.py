6a6b8bfa0ba70a6721418e0c533d2caa308623ee
Josh <laxisgood4u@gmail.com>
2020-06-28 22:20:20 -0400

    Login Manager

--- accounts/LoginManager.py
+++ accounts/LoginManager.py
@@ -1,34 +1,84 @@
+import array
+import json, os
+import random
+import string
+
 from passlib.context import CryptContext
 from pathlib import Path
+
+PASSWORD_VALUES = list(f'{string.ascii_letters} + {string.digits}')
+
+
+def update_json(file):
+	file.flush()
+	os.fsync(file.fileno())
+
+
+def create_password():
+	random.shuffle(PASSWORD_VALUES)
+	return ''.join(random.choices(PASSWORD_VALUES, k = 12))
 
 
 class LoginManager:
 
+	def __new__(cls):
+		return super().__new__(cls)
+
 	def __init__(self):
-		self.HASHES_PATH = Path("hashes.txt")
+		self.ACCOUNTS_PATH = Path("accounts.json")
+		self.ADMIN_USERNAME = "joshbl"
 		self.PWD_CONTEXT = CryptContext(
 			schemes = ["pbkdf2_sha256"],
 			default = "pbkdf2_sha256",
 			pbkdf2_sha256__default_rounds = 30000
 		)
-		self.saved_hashes = self.get_hashes()
+		self.check_json()
+		self.saved_accounts = self.get_accounts()
 
-	def get_hashes(self):
-		self.HASHES_PATH.touch()  # will create file, if it exists will do nothing
-		with open(self.HASHES_PATH) as file:
-			lines = []
-			for line in file:
-				lines.append(line.strip())
-			file.close()
-		return lines
+	def validate_request(self, endpoint, path, username, password):
+		return endpoint.__contains__(path) and self.check_encrypted_password(username, password)
 
-	def encrypt_password(self, password):
-		with open(self.HASHES_PATH, "a+") as file:
-			file.write(self.PWD_CONTEXT.hash(password))
+	def check_json(self):
+		if not self.ACCOUNTS_PATH.exists():
+			with open(self.ACCOUNTS_PATH, "w") as file:
+				file.write(json.dumps({}))
+				update_json(file)
+				file.close()
 
-	def check_encrypted_password(self, password):
-		if password is not None:
-			for hashed in self.saved_hashes:
-				if self.PWD_CONTEXT.verify(password, hashed):
-					return True
-		return False
+	def save_json(self):
+		with open(self.ACCOUNTS_PATH, "w") as json_file:
+			json_file.write(json.dumps(self.saved_accounts))
+			update_json(json_file)
+			json_file.close()
+
+	def get_accounts(self):
+		with open(self.ACCOUNTS_PATH) as hashes_json:
+			accounts = json.load(hashes_json)
+			hashes_json.close()
+			print(accounts)
+		return accounts
+
+	def create_account(self, username):
+		if not self.saved_accounts.__contains__(username):
+			password = create_password()
+			password_hash = self.PWD_CONTEXT.hash(password)
+			self.saved_accounts.update({username: password_hash})
+			self.save_json()
+			return "<h1>Account creation successful!</h1>" + "<p>Username: " + username + "\nPassword: " + password + "</p>"
+		return "<h1>Account username {" + username + "} already exists!</h1>"
+
+	def delete_account(self, username):
+		self.saved_accounts.pop(username)
+		self.save_json()
+		return "<h1>Account username {" + username + "} deleted.</h1>"
+
+	def is_admin(self, username):
+		return username == self.ADMIN_USERNAME
+
+	def check_encrypted_password(self, username, password):
+		try:
+			account_hash = self.saved_accounts[username]
+		except KeyError:
+			return False
+		return username is not None and password is not None and account_hash is not None and self.PWD_CONTEXT.verify(
+			password, account_hash)
