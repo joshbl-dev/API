152bd1d427b9c706ae0f705ea9103170a9a06171
Josh <ddiamonds@joshbl.dev>
2020-08-29 22:28:35 -0400

    error fix

--- Scheduler.py
+++ Scheduler.py
@@ -5,10 +5,6 @@ import schedule
 import os
 
 
-def send_quote_emails():
-    os.system("java -jar SchwartzBot.jar")
-    print("Sending out motivation emails!")
-
 
 def run_continuously(interval=1):
     cease_continuous_run = threading.Event()
@@ -31,5 +27,9 @@ class Scheduler:
         return super().__new__(cls)
 
     def __init__(self):
-        schedule.every().minute.do(send_quote_emails())
+        schedule.every().minute.do(self.send_quote_emails)
         run_continuously()
+
+    def send_quote_emails(self):
+        os.system("java -jar SchwartzBot.jar")
+        print("Sending out motivation emails!")
